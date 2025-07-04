3d2c2f96a72e8882f21dad6b46b5d16a4de9db57
nagira syunya <241004@std.hi-joho.ac.jp>
2025-07-04 12:35:46 +0900

    できました

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        Hero h = new Hero(40,"ミナト");
+        System.out.println(h.getName() + "の現在のHP: " + h.getHp());
+
+        King k = new King();
+        k.talk(h);
+
+        Inn i = new Inn();
+        System.out.println("宿屋で回復中...");
+        i.checkIn(h);
+        h.sleep();
+        System.out.println(h.getName() + "の現在のHP: " + h.getHp());
+        System.out.println(h.getName() + "は、眠って回復した！");
+
+        Matango m = new Matango("お化けキノコ",'A');
+        h.attack(m);
 
     }
 }
